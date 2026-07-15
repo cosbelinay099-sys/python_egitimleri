bfc63dece68b7cfe36a301ea9cd70e6db1bf52d4
cosbelinay099-sys <cosbelinay099@gmail.com>
2026-07-15 13:13:05 +0300

    Add files via upload
    değişkenler konusundaki öğrendiğim ilk çalışmalar

---
+++ veriables.py
@@ -0,0 +1,48 @@
+maasAli=5000
+maasAhmet=4000
+vergi=0.27
+
+
+
+print(maasAli-(maasAli*vergi))
+print(maasAhmet-(maasAhmet*vergi))
+
+#değişken tanımlama kuralları
+#sayısal ifade rakam ile başlayamaz
+
+number1=15
+print(number1)
+
+number1=22
+print(number1)
+
+number1 +=12
+print(number1)
+
+#büyük küçük harf duyarlılığı vardır
+
+age=20
+AGE=20
+print(age)
+print(AGE)
+
+# türkçe karakter kullanmayalım
+yaş=30
+yas=30
+print(yaş)
+print(yas)
+
+x = 1        #int
+y = 2.8      #float
+z = 1j       #complex
+name = "Belinay"  #string
+isStudent = True  #bool
+
+a ='10'
+b = '20'
+print(a+b) #1020
+
+
+firstName = "Belinay"
+lastName = "Coşkun"
+print(firstName + " " + lastName)  #Belinay Coşkun
