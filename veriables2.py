f730c7f849d684b24a49f9b5af57e79f8452824e
cosbelinay099-sys <cosbelinay099@gmail.com>
2026-07-15 20:07:51 +0300

    Add files via upload
    ilk python deneme örnekleri2

---
+++ veriables2.py
@@ -0,0 +1,23 @@
+müşteriadi="Belinay"
+müşterisoyadi= "Coşkun"
+müştericinsiyeti="Kadın"
+müşteritckimliknumarasi= "12345678901"
+müşteridoğumtarihi= "25.09.2006"
+müşteriadres= "İstanbul"
+müşteritelefonnumarasi= "0532 123 45 67"
+müşteriyaşı= 20
+
+
+print(müşteriadi + " " + müşterisoyadi + " " + müştericinsiyeti)
+print(müşteritckimliknumarasi + " " + müşteridoğumtarihi + " " + müşteriadres)
+print(müşteritelefonnumarasi + " " + str(müşteriyaşı))
+
+
+
+#2.örnek siparişlerin toplam bilgisi
+
+order1=110  
+order2=253.65 
+order3= 75.50 
+hesap= order1 + order2 + order3
+print("Toplam sipariş tutarı: " + str(hesap) + " TL")
