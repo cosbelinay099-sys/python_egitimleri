856566c8f92d53617912320df29c1597fa8268bb
cosbelinay099-sys <cosbelinay099@gmail.com>
2026-07-15 21:02:48 +0300

    Add files via upload
    daire alan çevre hesaplama

---
+++ type-coversion-demo.py
@@ -0,0 +1,7 @@
+daireyarıçap=input("Dairenin yarıçapını giriniz: ")
+daireyarıçap=int(daireyarıçap)
+print(daireyarıçap)
+dairealan=3.14*daireyarıçap**2
+print("Dairenin alanı: ", dairealan)
+dairecevre=2*3.14*daireyarıçap
+print("Dairenin çevresi: ", dairecevre)
