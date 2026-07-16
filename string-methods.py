f63eab094823bccf053b2f8c5af5c9df207367cf
cosbelinay099-sys <cosbelinay099@gmail.com>
2026-07-16 14:47:09 +0300

    Add files via upload

---
+++ string-methods.py
@@ -0,0 +1,24 @@
+message=' Hello There. My name is Belinay Sude'
+#message=message.upper() #tüm karakterleri büyük harfe çevirir
+#message=message.lower() #tüm karakterleri küçük harfe çevirir
+#message=message.title() #her kelimenin ilk harfini büyük yapar
+#message=message.capitalize() #cümlenin ilk harfini büyük yapar
+#message=message.strip() #başındaki ve sonundaki boşlukları siler
+#message=message.split() #cümleyi kelimelere ayırır ve listeye çevirir
+#message=message.replace('Belinay Sude','Belinay') #cümledeki bir kelimeyi değiştirir
+#message=message.find('Belinay') #cümledeki bir kelimenin index numarasını verir
+#message='---'.join(message) #cümledeki tüm karakterleri belirli bir karakter ile birleştirir
+#index=message.find('Belinay') #cümledeki bir kelimenin index numarasını verir
+#isFound=message.startswith('Hello') #cümle belirli bir kelime ile başlıyorsa True, başlamıyorsa False döndürür
+#isFound=message.endswith('Sude') #cümle belirli bir kelime ile bitiyorsa True, bitmiyorsa False döndürür
+
+message=message.center(59,'*') #cümleyi belirli bir karakter sayısına göre ortalar
+
+
+
+
+
+
+
+print(message)
+
