5a9711f7383307bf4273f30afa653401237191b1
cosbelinay099-sys <cosbelinay099@gmail.com>
2026-07-16 14:24:31 +0300

    Add files via upload

---
+++ strings-demo.py
@@ -0,0 +1,34 @@
+website = "https://www.python.org"
+course= "Python Programming"
+greeting= 'website: ' + website + ' \ncourse: ' + course    
+
+print(len(course))
+print(len(website))
+print(course[:15])
+print(course[-15:])
+print(website[:15])
+print(website[-15:])
+print(course[::-1]) #cümleyi tersten yazdırır
+print(website[7:11])
+
+
+
+name='Belinay'
+surname='Coşkun'
+age=20
+job='Mühendis'
+print(f"My name is {name} {surname} and I am {age} years old. I work as a {job}.")  #f-string ile değişkeni cümleye ekleme
+
+s='Hello World'
+s= s[0:6]+ 'W'+ s[-4:]
+print(s)
+
+result='abc'*3
+print(result)
+
+
+
+
+
+
+
